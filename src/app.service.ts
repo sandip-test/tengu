39df0a9c1e93b130d003c0b951675ad873d6eb2c
Sandip Sapkota <sandipsapkota9844@gmail.com>
2025-07-21 21:46:26 +0545

    minor fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { APP_CONFIG } from './config';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  returnAPIDetails() {
+    return {
+      name: APP_CONFIG.NAME,
+      version: APP_CONFIG.CURRENT_VERSION,
+      docs:
+        process.env.ENABLE_SWAGGER === 'true' ? '/docs' : 'No Documentation',
+      environment: process.env.NODE_ENV || 'development',
+    };
   }
 }
